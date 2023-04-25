8fb9f008b3ae0009d1598bba2787889701a70a36
Itay <itay3742@gmail.com>
2023-04-25 16:34:53 +0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,12 +1,16 @@
 from routes.count import bp as count
 from routes.homepage import bp as home
+from routes.table import bp as logs_table
 import logging
 from database.database import app
 
-logging.basicConfig(filename='./logs/record.log', level=logging.DEBUG, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
+# logging.basicConfig(filename='./logs/record.log', level=logging.DEBUG, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
+logging.basicConfig(level=logging.DEBUG)
+app.logger.info("hello world")
 
 app.register_blueprint(home)
 app.register_blueprint(count)
+app.register_blueprint(logs_table)
 
 
 if __name__ == '__main__':
